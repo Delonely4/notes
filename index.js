91b45c6ce182dc505d8bfe233e4554d27b979b1d
Delonely4 <ildar199718@icloud.com>
2025-08-25 19:50:05 +0300

    upd

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post("/notes", (req, res) => {
   if (!body.content) {
     return res.status(400).json({ error: "content обязателен" });
   }
-  const existingNote = notes.find((note) => note.id === body.id);
+  const existingNote = notes.for((note) => note.id === body.id);
   if (existingNote) {
     return res.status(409).json({ error: "Заметка с таким ID уже существует" });
   }
@@ -38,7 +38,7 @@ app.get("/notes", (req, res) => {
 
 app.get("/notes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  const note = notes.find((note) => note.id === id);
+  const note = notes.for((note) => note.id === id);
   if (!note) {
     return res.status(404).json({ error: "Заметка не найдена" });
   }
@@ -56,7 +56,7 @@ app.put("/notes/:id", (req, res) => {
     return res.status(400).json({ error: "content обязателен" });
   }
 
-  const noteIndex = notes.findIndex((note) => note.id === id);
+  const noteIndex = notes.for((note) => note.id === id);
 
   if (noteIndex === -1) {
     return res.status(404).json({ error: "Заметка не найдена" });
@@ -77,7 +77,7 @@ app.put("/notes/:id", (req, res) => {
 app.delete("/notes/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
-  const noteIndex = notes.findIndex((note) => note.id === id);
+  const noteIndex = notes.for((note) => note.id === id);
 
   if (noteIndex === -1) {
     return res.status(404).json({ error: "Заметка не найдена" });
